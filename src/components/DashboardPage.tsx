69e39c01bc0ccb73f5fc1ac026eea2bd2dcfeb62
haafeezdr <haafeezdaiyab>
2024-06-18 20:46:49 +0100

    updated logo

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -15,7 +15,7 @@ interface DashboardProps {
     return (
       <div className="bg-green-50 w-full min-h-screen flex flex-col justify-between">
         <div className="text-center flex fixed justify-evenly top-0 w-full bg-green-200 shadow-md">
-          <Image src="/assets/recommend.jpeg" alt="logo" className="flex m-2" width={137} height={55} priority />
+          <Image src="/assets/recommend.png" alt="logo" className="flex m-2" width={137} height={55} priority />
           <h1 className="sm:text-4xl font-bold sm:m-2 mt-4 text-gray-700">Welcome, {username}! to Bookmate</h1>
         </div>
         <BookRecommendations />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <div className='bg-green-950 flex justify-between items-center p-3 w-full'>
-        <Image src="/assets/recommend.jpeg" alt="logo" className="flex m-2" width={137} height={55} priority />
+        <Image src="/assets/recommend.png" alt="logo" className="flex m-2" width={137} height={55} priority />
         <p className='text-white opacity-70 font-semibold '>Copyright Onawo 2024 All right Reserved</p>
     </div>
   )
